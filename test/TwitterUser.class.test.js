96625cdc2368647fe4f89b26af2ae1e98a4a05fe
Matthias Jahn <matthias.jahn@roomieplanet.de>
2012-04-05 15:25:11 +0200

    Refactor TwitterUser

--- test/TwitterUser.class.test.js
+++ test/TwitterUser.class.test.js
@@ -10,54 +10,11 @@ var expect                  = require("expect.js"),
     accessTokenSecretDummy  = everyauthMock.twitter.findOrCreateUserCallbackParams.accessTokenSecret;
 
 describe("TwitterUser", function () {
-
-    describe("#__construct & #__isTwitterUserData", function () {
-        it("should throw an error without giving twitterUserData", function () {
-            expect(function () {
-                new TwitterUser();
-            }).to.throwException();
-        });
-        it("should throw an error giving any object", function () {
-            expect(function () {
-                new TwitterUser({});
-            }).to.throwException();
-        });
-        it("should be an instance of TwitterUser", function () {
-            expect(new TwitterUser(twitterUserDataMock)).to.be.a(TwitterUser);
-        });
+    
+    before(function () { 
+        twitterUser = new TwitterUser(twitterUserDataMock);
     });
-
-    twitterUser = new TwitterUser(twitterUserDataMock);
-    describe("#getId", function () {
-        it("should return an equal id to twitterUserDataMock.id", function () {
-            expect(twitterUser.getId()).to.be.equal(twitterUserDataMock.id);
-        });
-    });
-
-    describe("#getFriendsCount", function () {
-        it("should return a count equal to twitterUserDataMock.friends_count.", function () {
-            expect(twitterUser.getFriendsCount()).to.be.equal(twitterUserDataMock.friends_count);
-        });
-    });
-
-    describe("#getScreenName", function () {
-        it("should return a screenname equal to twitterUserDataMock.screen_name", function () {
-            expect(twitterUser.getScreenName()).to.be.equal(twitterUserDataMock.screen_name);
-        });
-    });
-
-    describe("#getName", function () {
-        it("should return a name equal to twitterUserDataMock.name", function () {
-            expect(twitterUser.getName()).to.be.equal(twitterUserDataMock.name);
-        });
-    });
-
-    describe("#getFollowersCount", function () {
-        it("should return a count equal to twitterUserDataMock.followers_count.", function () {
-            expect(twitterUser.getFollowersCount()).to.be.equal(twitterUserDataMock.followers_count);
-        });
-    });
-
+    
     describe("#setSession", function () {
         it("should return a reference to twitterUser", function () {
             expect(twitterUser.setSession(sessionDummy)).to.be.equal(twitterUser);
