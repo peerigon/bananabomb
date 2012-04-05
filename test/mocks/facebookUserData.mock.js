b2fb209878e73423d9bbdfbce716e67545a565fe
Matthias Jahn <matthias.jahn@roomieplanet.de>
2012-04-05 14:05:55 +0200

    Add facebookUserData.mock

---
+++ test/mocks/facebookUserData.mock.js
@@ -0,0 +1,19 @@
+//"use strict";
+
+module.exports = {
+    
+    accessToken: "Asafsda-aASDfgsahafweaFdafjdalösdklfajsohfowhflasödjfksdöjflasflsBadsfasDFSADFGLGSDAGLDSAHGLSÖFLSJAJFDLAFAasfsaa",
+    expires: "5097820",
+    id: "100005277437137",
+    name: "Test Er",
+    first_name: "Test",
+    last_name: "Er",
+    link: "http://www.facebook.com/profile.php?id=100005277437137",
+    gender: "male",
+    email: "mail@fasdfsdf.de",
+    timezone: 1,
+    locale: "de_DE",
+    verified: true,
+    updated_time: "2012-02-22T12:44:36+0000" 
+
+};
