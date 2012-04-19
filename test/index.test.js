6e7e353b4543af77b5baa996ed8a8a84f8de7bb3
Matthias Jahn <matthias.jahn@roomieplanet.de>
2012-04-19 08:40:24 +0200

    Remove test for TwitterUser, TwitterConsumer, FacebookUser, FacebookConsumer

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,7 @@ var _ = require("underscore"),
     expect = require("expect.js"),
     indexReference = {
         twitter: {
-            Consumer: require("../lib/twitter/TwitterConsumer.class.js"),
-            RESTClient: require("../lib/twitter/TwitterRESTClient.class.js"),
-            User: require("../lib/twitter/TwitterUser.class.js")
+            RESTClient: require("../lib/twitter/TwitterRESTClient.class.js")
         },
         email: {
            EMail: require("../lib/email/EMail.class.js"),
@@ -14,9 +12,7 @@ var _ = require("underscore"),
            SMTPClient: require("../lib/email/SMTPClient.class.js")
         },
         facebook: {
-            Consumer: require("../lib/facebook/FacebookConsumer.class.js"),
-            GraphAPI: require("../lib/facebook/FacebookGraphAPI.class.js"),
-            User: require("../lib/facebook/FacebookUser.class.js")
+            GraphAPI: require("../lib/facebook/FacebookGraphAPI.class.js")
         }
 
     },
