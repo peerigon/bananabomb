033a194a34e2d708aa7f1c32fb9e9a7831f86bed
Matthias Jahn <matthias.jahn@roomieplanet.de>
2012-04-19 08:30:22 +0200

    Removed tests for TwitterUser, TwitterConsumer, TwitterEveryatuh

--- test/Facebook.class.test.js
+++ test/Facebook.class.test.js
@@ -2,62 +2,58 @@
 
 var _                       = require("underscore"),
     expect                  = require("expect.js"),
-    FacebookUser            = require("../lib/facebook/FacebookUser.class"),
+
     Facebook                = require("../lib/facebook/FacebookGraphAPI.class"),
-    FacebookConsumer        = require("../lib/facebook/FacebookConsumer.class"),
+
     facebookDocuReference   = require("./mocks/facebookDocuReference.mock"),
     facebookUserDataMock    = require("./mocks/facebookUserData.mock"),
     httpsMock               = require("./mocks/https.mock"),
     error                   = function () { /* error */ },
-    success                 = function () { /* do nothing */ };
-    
+    success                 = function () { /* do nothing */ },
+    facebookUserID          = facebookUserDataMock.id,
+    facebookUserAccessToken = facebookUserDataMock.AccessToken;
+
 
 describe("Facebook", function () {
-    
-    describe("Non-Facebook-API Tests", function () {
-        
-        var facebookUser,
-            facebookConsumer,
-            facebook;
-            
-        before(function () {
-            facebookUser    = new FacebookUser(facebookUserDataMock);
-            facebookUser
-                .setAccessToken(facebookDocuReference.accessToken)
-                .setAccessTokExtra(facebookDocuReference.accessTokExtra);
-            facebookConsumer = new FacebookConsumer(facebookDocuReference.appId, facebookDocuReference.appSecret);
-            
-        });
-
-        describe("#__construct", function () {
-            it("should throw an error without providing instances of FacebookUser, FacebookConsumer", function () {
-                expect(function() {new Facebook();}).to.throwException();
-            });
-        
-            it("should be an instance of Facebook", function () {
-                facebook = new Facebook(FacebookUser, FacebookConsumer);
-                expect(facebook).to.be.a(Facebook);
-            });
-        });
-        
-    });
-    
-    
+//    
+//    describe("Non-Facebook-API Tests", function () {
+//        
+//        var facebookUser,
+//            facebookConsumer,
+//            facebook;
+//            
+//        before(function () {
+//            facebookUser    = new FacebookUser(facebookUserDataMock);
+//            facebookUser
+//                .setAccessToken(facebookDocuReference.accessToken)
+//                .setAccessTokExtra(facebookDocuReference.accessTokExtra);
+//            facebookConsumer = new FacebookConsumer(facebookDocuReference.appId, facebookDocuReference.appSecret);
+//            
+//        });
+//
+//        describe("#__construct", function () {
+//            it("should throw an error without providing instances of FacebookUser, FacebookConsumer", function () {
+//                expect(function() {new Facebook();}).to.throwException();
+//            });
+//        
+//            it("should be an instance of Facebook", function () {
+//                facebook = new Facebook(FacebookUser, FacebookConsumer);
+//                expect(facebook).to.be.a(Facebook);
+//            });
+//        });
+//        
+//    });
+//    
+//    
     describe("Facebook-API Test", function () {
         
         describe("#getStatuses", function () {
             
-            var facebook,
-                facebookUser,
-                facebookConsumer;
+            var facebook;
                 
             before(function() {
-                facebookUser        = new FacebookUser(facebookUserDataMock);
-                facebookUser
-                    .setAccessToken(facebookDocuReference.accessToken)
-                    .setAccessTokExtra(facebookDocuReference.accessTokExtra);
-                facebookConsumer    = new FacebookConsumer(facebookDocuReference.appId, facebookDocuReference.appSecret);
-                facebook = new Facebook(facebookUser, facebookConsumer, httpsMock);
+
+                facebook = new Facebook(facebookUserID, facebookUserAccessToken, httpsMock);
                 facebook.getStatuses(
                     error,
                     success
@@ -69,17 +65,11 @@ describe("Facebook", function () {
         
         describe("#postStatus", function () {
             
-            var facebook,
-                facebookUser,
-                facebookConsumer;
+            var facebook;
                 
             before(function () {
-                facebookUser        = new FacebookUser(facebookUserDataMock);
-                facebookUser
-                    .setAccessToken(facebookDocuReference.accessToken)
-                    .setAccessTokExtra(facebookDocuReference.accessTokExtra);
-                facebookConsumer    = new FacebookConsumer(facebookDocuReference.appId, facebookDocuReference.appSecret);
-                facebook = new Facebook(facebookUser, facebookConsumer, httpsMock);
+ 
+                facebook = new Facebook(facebookUserID, facebookUserAccessToken, httpsMock);
                 facebook.postStatus(
                     error,
                     success,
@@ -91,17 +81,15 @@ describe("Facebook", function () {
         
         describe("#deleteStatus", function () {
             
-            var facebook,
-                facebookUser,
-                facebookConsumer;
+            var facebook;
                 
             before(function () {
-                facebookUser        = new FacebookUser(facebookUserDataMock);
-                facebookUser
-                    .setAccessToken(facebookDocuReference.accessToken)
-                    .setAccessTokExtra(facebookDocuReference.accessTokExtra);
-                facebookConsumer    = new FacebookConsumer(facebookDocuReference.appId, facebookDocuReference.appSecret);
-                facebook = new Facebook(facebookUser, facebookConsumer, httpsMock);
+
+                facebook = new Facebook(facebookUserID, facebookUserAccessToken, httpsMock);
+                facebook.deleteStatus(
+                    error,
+                    success,
+                    facebookDocuReference.status.statusId)
             });
                         
 //            describe("Error handling", function () {
